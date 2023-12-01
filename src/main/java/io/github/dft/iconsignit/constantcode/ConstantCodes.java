2c273708780bd26c222603f831f4b0c8a92763f5
Manan Thakkar <manan.thakkar@dakshyafreondtech.com>
2023-12-01 18:19:08 +0530

    Added rate request/response pojo and added rate API

--- src/main/java/io/github/dft/iconsignit/constantcode/ConstantCodes.java
+++ src/main/java/io/github/dft/iconsignit/constantcode/ConstantCodes.java
@@ -1,9 +1,9 @@
 package io.github.dft.iconsignit.constantcode;
 
 public interface ConstantCodes {
-    String CONTENT_TYPE = "Content-Type";
     String ACCEPT = "Accept";
     String HTTPS = "https://";
+    String CONTENT_TYPE = "Content-Type";
     String BASE_ENDPOINT = "%s.iconsignit.com.au/api/";
     String HTTP_REQUEST_CONTENT_TYPE_JOSN = "application/json";
 }
